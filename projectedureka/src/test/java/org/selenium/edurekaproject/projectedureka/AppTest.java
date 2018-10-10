d5396f30a250fda6a2bf2461aecf423565107a07
jayaramreddyrajandhla <jayaramreddyrajandhla@192.168.0.16>
2018-10-10 21:54:38 +0100

    initial commit to repo

---
+++ projectedureka/src/test/java/org/selenium/edurekaproject/projectedureka/AppTest.java
@@ -0,0 +1,14 @@
+package org.selenium.edurekaproject.projectedureka;
+
+import org.testng.annotations.Test;
+
+public class AppTest 
+    
+{
+    @Test
+    public void firstTest()
+    {
+    	System.out.println("This is maven project");
+    }
+    
+}
