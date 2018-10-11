c28563dc0d8aa3fe071f50ec8160c152b7cd5d52
jayaramreddyrajandhla <jayaramreddyrajandhla@192.168.0.16>
2018-10-11 13:58:57 +0100

    initial commit to workspace to central repo

---
+++ myMaven/src/test/java/org/selenium/myMaven/myMaven/AppTest.java
@@ -0,0 +1,18 @@
+package org.selenium.myMaven.myMaven;
+
+import org.testng.annotations.Test;
+
+public class AppTest 
+    
+{
+	@Test
+	public static void apptest()
+	{
+		System.out.println("My Maven project");
+	}
+	@Test
+	public static void another() 
+	{
+		System.out.println("Another print");
+	}
+}
